e0a53cf4a0bd5985e7b40e7b0815504b586f0775
Paulo Amaral <ppauloces27@gmail.com>
2021-12-19 05:52:09 -0300

    formulario completo si senho

--- contato.php
+++ contato.php
@@ -7,16 +7,17 @@ $email = addslashes($_POST['email']);
 $mensagem = addslashes($_POST['message']);
 $category = addslashes($_POST['category']);
 
+
 $to = "ppauloces27@gmail.com";
 $assunto = "Contato - UpNetwork";
-$body = "Nome: ".$nome. "\r\n" . "Email: ".$email."\r\n" . "Categoria: ".$category."\r\n". "Mensagem: ".$mensagem;
+$body = "Nome: ".$nome. "\r\n" . "Email: ".$email."\r\n" . "Categoria: ". $category ."\r\n". "Mensagem: ".$mensagem;
 
-$header = "From:chegadevicio894@gmail.com"."\r\n"."Reply-To:".$email."\r\n"."X=Mailer:PHP/".phpversion();
+$header = "From:ppauloces27@gmail.com"."\r\n"."Reply-To:".$email."\r\n"."X-Mailer:PHP/".phpversion();
 
-if(mail($to,$subject,$body,$header)){
-    echo("Email enviado com sucesso");
+if(mail($to, $assunto, $body, $header)){
+    echo "<p style='text-align: center; color: green;'><br>Email enviado com sucesso</p>";
 }else{
-    echo("O email não pode ser enviado");
+    echo "<p style='text-align: center; color: red;'><br>O email não pode ser enviado</p>";
 }
 
 
